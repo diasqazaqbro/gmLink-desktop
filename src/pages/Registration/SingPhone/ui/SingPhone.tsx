550ea0f3ed348f64212f37bff9aa7ab902af8821
Nruzhzhaugash <clonn262@gmail.com>
2023-11-15 17:51:31 +0600

    I added some site pages, changed what was needed and deleted what was not needed

--- src/pages/Registration/SingPhone/ui/SingPhone.tsx
+++ src/pages/Registration/SingPhone/ui/SingPhone.tsx
@@ -1,3 +1,33 @@
+import sass from './SingPhone.module.sass'
 export function SingPhone() {
-  return <>SingPhone</>;
+  return <>
+    <div className={sass.container}>
+      <div className={sass.whiteBlock}>
+        <img src="./assets/white.svg" alt="" />
+      </div>
+      <div className={sass.spBadges}>
+        <a href="" className={sass.arrow}><img src="./assets/arrow_back_ios.svg" alt="arrow" /></a>
+        <a href="" className={sass.help}><img src="./assets/help.svg" alt="" /></a>
+      </div>
+      <div className={sass.spInner}>
+        <h1 className={sass.spTitle}>Please enter your Email</h1>
+        <div className={sass.spDraw}>
+          <img src="./assets/enter__email-main.svg" alt="" />
+        </div>
+        <form action="" className={sass.spAuth}>
+        <div className={sass.enterData}>
+          <input   name="search_query"  aria-label="Search" role="combobox" aria-haspopup="false" aria-autocomplete="list" dir="ltr" type="number" id="Input" value="" className={sass.enterEmail} placeholder="Enter your number" />
+          <button className={sass.clearData}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
+              <circle cx="8.99999" cy="8.49999" r="8.49999" fill="#D9D9D9"/>
+              <path d="M9 9.71429L5.35714 13.3571L4.14286 12.1429L7.78571 8.5L4.14286 4.85714L5.35714 3.64286L9 7.28571L12.6429 3.64286L13.8571 4.85714L10.2143 8.5L13.8571 12.1429L12.6429 13.3571L9 9.71429Z" fill="#1C1B1F"/>
+            </svg>
+          </button>
+        </div>
+        <button className={sass.spBtn}>Next</button>
+        </form>
+        <a href="" className={sass.spContact}>Contact Us</a>
+      </div>
+    </div>
+  </>;
 }
--- src/pages/Survey/SGender/ui/SGender.tsx
+++ src/pages/Survey/SGender/ui/SGender.tsx
@@ -1,3 +1,41 @@
+import sass from './SGender.module.sass'
+
 export function SGengder() {
-  return <>SGender</>;
+  return <>
+    <div className={sass.container}>
+      <div className={sass.sgBadges}>
+        <a href="" className={sass.arrow}><img src="./assets/arrow_back_ios.svg" alt="arrow" /></a>
+        <a href="" className={sass.help} ><img src="./assets/help.svg" alt="help" /></a>
+      </div>
+      <div className={sass.sgInner}>
+        <div className={sass.sgText}>
+          <h1>You gender...</h1>
+          <p>This information is needed to correctly determine the pair for you.</p>
+        </div>
+        <div className={sass.sgChoice}>
+          <button className={sass.male}>Male</button>
+          <button className={sass.female}>Female</button>
+        </div>
+          <div className={sass.sgChoice2}>
+            <span>or</span>
+            <button className={sass.other}>Other</button>
+          </div>
+          <form action="" className={sass.sgInput}>
+            <div className={sass.gcEt}>
+              <input className={sass.gcEnter} type="text" placeholder="Enter if other"/>
+              <a href="" className={sass.gcClear}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
+                  <circle cx="8.54296" cy="8.49999" r="8.49999" fill="#D9D9D9"/>
+                  <path d="M8.54297 9.71429L4.90011 13.3571L3.68583 12.1429L7.32868 8.5L3.68583 4.85714L4.90011 3.64286L8.54297 7.28571L12.1858 3.64286L13.4001 4.85714L9.75725 8.5L13.4001 12.1429L12.1858 13.3571L8.54297 9.71429Z" fill="#1C1B1F"/>
+                </svg>
+              </a>
+            </div>
+            <button className={sass.nextBtn}>Next</button>
+          </form>
+          <div className={sass.sgContact}>
+            <a href="">Contact Us</a>
+          </div>
+      </div>
+    </div>
+  </>;
 }
