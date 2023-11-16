6d224992d9aaab9adbc9260c9bff9cf081e4c024
Nruzhzhaugash <clonn262@gmail.com>
2023-11-16 13:02:30 +0600

    changed pages to fit iPhone sizes

--- src/pages/Main/ui/MainPage.tsx
+++ src/pages/Main/ui/MainPage.tsx
@@ -1,6 +1,4 @@
 import sass from './MainPage.module.sass';
-import { SingPhone } from '../../Registration/SingPhone/ui/SingPhone'
-import { SingEmail } from '../../Registration/SingEmail/ui/SingEmail'
 
 export function MainPage() {
 
@@ -24,8 +22,8 @@ export function MainPage() {
             <img src="/src/pages/Main/ui/assets/mP--people.svg" alt="People" />
           </div>
           <div className={sass.mpBtns}>
-            <button onClick={handleJoinButtonClick} className={sass.mpBtn}>Sign in Number</button>
-            <button onClick={handleButton} className={sass.mpBtn}>Sign in Email</button>
+            <button className={sass.mpBtn}>Sign in Number</button>
+            <button className={sass.mpBtn}>Sign in Email</button>
           </div>
           <ul className={sass.mpLinks}>
             <li className={sass.mpLink}>
